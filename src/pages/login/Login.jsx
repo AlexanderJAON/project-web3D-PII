213bd43c3930dc36e14408e9043d6d10e3a19281
Jhon Alexander Osma <jhon.osma@correounivalle.edu.co>
2024-09-05 17:36:30 -0500

    Improved login and project header logic

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,21 +1,42 @@
 import { useCallback } from "react";
 import useAuthStore from "../../stores/use-auth-store";
 import { useEffect } from "react";
+import "./Login.css";
 
-export default function Login(){
-    const {user, loginGoogleWithPopUp, logout, observeAuthState}= useAuthStore();
-    const handleLogin=useCallback(()=>{
-        loginGoogleWithPopUp();
-    },[loginGoogleWithPopUp])
-    useEffect(()=>{
-        observeAuthState();
-        console.log(user)
-    }, [observeAuthState,user])
-    return (
+export default function Login() {
+  const { user, loginGoogleWithPopUp, logout, observeAuthState } =
+    useAuthStore();
+
+  const handleLogin = useCallback(() => {
+    loginGoogleWithPopUp();
+  }, [loginGoogleWithPopUp]);
+
+  const handleLogout = useCallback(() => {
+    logout();
+  }, [logout]);
+
+  useEffect(() => {
+    observeAuthState();
+    console.log(user);
+  }, [observeAuthState, user, handleLogin]);
+
+  return (
+    <div className="container-login">
+      {user ? (
         <>
-        <div className="container-login">
-            <button onClick={handleLogin}>Login</button>
-        </div>
+          <p className="welcome-text">Bienvenido, {user.displayName}</p>
+          <button className="button-logout" onClick={handleLogout}>
+            Cerrar Sesión
+          </button>
         </>
-    )
+      ) : (
+        <>
+          <p className="welcome-text">Inicia sesión para continuar</p>
+          <button className="button-login" onClick={handleLogin}>
+            Iniciar Sesión
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
