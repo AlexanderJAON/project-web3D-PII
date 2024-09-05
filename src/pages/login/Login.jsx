e97e3feb48c49417776fac89b6a7a4bb539d6c7f
RaquelGG-UV <raquel.gonzalez@correounivalle.edu.co>
2024-09-05 08:57:18 -0500

    The pages folder was created and the store folder was modified

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,21 @@
+import { useCallback } from "react";
+import useAuthStore from "../../stores/use-auth-store";
+import { useEffect } from "react";
+
+export default function Login(){
+    const {user, loginGoogleWithPopUp, logout, observeAuthState}= useAuthStore();
+    const handleLogin=useCallback(()=>{
+        loginGoogleWithPopUp();
+    },[loginGoogleWithPopUp])
+    useEffect(()=>{
+        observeAuthState();
+        console.log(user)
+    }, [observeAuthState,user])
+    return (
+        <>
+        <div className="container-login">
+            <button onClick={handleLogin}>Login</button>
+        </div>
+        </>
+    )
+}
