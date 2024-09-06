9786eb399092b45c9c19e4dbf1e36db2a6acede5
Jhon Alexander Osma <jhon.osma@correounivalle.edu.co>
2024-09-06 15:41:47 -0500

    cosine animation and arrangement of moon.jsx parameters so that it can be referenced

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,37 +1,46 @@
-import { useCallback } from "react";
+import React, { useCallback, useRef } from "react";
 import useAuthStore from "../../stores/use-auth-store";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import Moon from "./models/Moon";
 import { OrbitControls } from "@react-three/drei";
+
 const Home = () => {
-    const { user, logout } =
-    useAuthStore();
+  const { user, logout } = useAuthStore();
 
-    const handleLogout = useCallback(() => {
-        logout();
-      }, [logout]);
+  const handleLogout = useCallback(() => {
+    logout();
+  }, [logout]);
 
-    
-    return(
-        <>
-        <p className="welcome-text">Bienvenido, {user.displayName}</p>
-          <button className="button-logout" onClick={handleLogout}>
-            Cerrar Sesión
-          </button>
-         <div className="container">
-         <Canvas>
-        <ambientLight intensity={1.5} />
-        <directionalLight position={[0, 10, 10]} intensity={5} />
-        <OrbitControls />
-          <Moon />
-           
-        </Canvas> 
-         </div>
-       
-        
-        </>
-        
-    )
-}
+  function CosineAnimation() {
+    const ref = useRef();
+    const frequency =4;
+
+    useFrame(({ clock }) => {
+      const time = clock.getElapsedTime();
+      if (ref.current) {
+        ref.current.position.y = Math.cos(frequency * time);
+      }
+    });
+
+    return <Moon ref={ref} />;
+  }
+
+  return (
+    <>
+      <p className="welcome-text">Bienvenido, {user.displayName}</p>
+      <button className="button-logout" onClick={handleLogout}>
+        Cerrar Sesión
+      </button>
+      <div className="container">
+        <Canvas>
+          <ambientLight intensity={1.5} />
+          <directionalLight position={[0, 10, 10]} intensity={5} />
+          <OrbitControls />  
+          <CosineAnimation />
+        </Canvas>
+      </div>
+    </>
+  );
+};
 
 export default Home;
